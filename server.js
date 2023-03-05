1b56c2195530964c44437adab3ca2f04196b4c6c
Bhupendra Maithele <bmaithele@gmail.com>
2023-03-06 01:30:35 +0530

    add condition

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const port = process.env.PORT || 3000
 app.get('/:id', function (req, res) {
     var filePath = "/assests/certificate.pdf";
     const id = req.params.id
-    fs.readFile(__dirname + filePath , function (err, data){
-        res.contentType("application/pdf");
-        res.send(data);
-    });
+    if (id === 'bd1fTj') {
+        fs.readFile(__dirname + filePath , function (err, data){
+            res.contentType("application/pdf");
+            res.send(data);
+        });
+    } else {
+        res.status(400).json({error: "NOT_FOUND_ERR"})
+    }
 });
 
 app.listen(port, () => {
