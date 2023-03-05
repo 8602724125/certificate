7ef80cd7f1acce0da977f1a6039ce804183facd0
Bhupendra Maithele <bmaithele@gmail.com>
2023-03-06 01:10:41 +0530

    change route

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
+app.get('/:id', function (req, res) {
     var filePath = "/assests/certificate.pdf";
+    const id = req.params.id
     fs.readFile(__dirname + filePath , function (err, data){
         res.contentType("application/pdf");
         res.send(data);
